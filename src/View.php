563e9ca5c3afeb040825611c00dd141ea2a5e93f
Alain Schlesser <alain.schlesser@gmail.com>
2017-08-11 17:26:04 +0200

    Extract path handling in View class into new PathHelper class.

--- src/View.php
+++ src/View.php
@@ -33,20 +33,6 @@ class View implements Renderable {
 	const VIEW_EXTENSION = 'php';
 
 	/**
-	 * Regex pattern to detect whether an URI is absolute or not.
-	 *
-	 * Note: This is taken from Drupal 7 code.
-	 *
-	 * @since 0.1.0
-	 */
-	const ABSOLUTE_PATH_PATTERN =
-		"/^(?:ftp|https?|feed):\/\/(?:(?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]"
-		. "{2})+:)*(?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@)?(?:(?:[a-z0-9"
-		. "\-\.]|%[0-9a-f]{2})+|(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\]))("
-		. "?::[0-9]+)?(?:[\/|\?](?:[\w#!:\.\?\+=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f"
-		. "]{2})*)?$/xi";
-
-	/**
 	 * URI to the view file to render.
 	 *
 	 * @since 0.1.0
@@ -119,67 +105,13 @@ class View implements Renderable {
 	 * @throws Exception\InvalidURI If an invalid URI was passed into the View.
 	 */
 	protected function validate( $uri ) {
-		$uri = $this->check_extension( $uri );
-		$uri = $this->make_absolute( $uri );
+		$uri = PathHelper::check_extension( $uri, static::VIEW_EXTENSION );
+		$uri = PathHelper::make_absolute( $uri, AS_SPEAKING_DIR );
 
 		if ( ! is_readable( $uri ) ) {
 			throw Exception\InvalidURI::from_uri( $uri );
 		}
 
 		return $uri;
-	}
-
-	/**
-	 * Check that the URI has the correct extension.
-	 *
-	 * Optionally adds the extension if none was detected.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string $uri URI to check the extension of.
-	 *
-	 * @return string URI with correct extension.
-	 */
-	protected function check_extension( $uri ) {
-		$extension = pathinfo( $uri, PATHINFO_EXTENSION );
-
-		if ( static::VIEW_EXTENSION !== $extension ) {
-			$uri .= '.' . static::VIEW_EXTENSION;
-		}
-
-		return $uri;
-	}
-
-	/**
-	 * Turn a relative URI into an absolute URI.
-	 *
-	 * If the URI is detected as being relative, it is assumed to be relative
-	 * to the plugin root folder.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string $uri URI to make absolute.
-	 *
-	 * @return string Absolute URI.
-	 */
-	protected function make_absolute( $uri ) {
-		if ( ! $this->is_absolute_path( $uri ) ) {
-			$uri = AS_SPEAKING_DIR . $uri;
-		}
-
-		return $uri;
-	}
-
-	/**
-	 * Check whether a given URI is absolute or not.
-	 *
-	 * @since 0.1.0
-	 *
-	 * @param string $uri URI to check.
-	 *
-	 * @return bool Whether the URI is absolute or not.
-	 */
-	protected function is_absolute_path( $uri ) {
-		return (bool) preg_match( static::ABSOLUTE_PATH_PATTERN, $uri );
 	}
 }
