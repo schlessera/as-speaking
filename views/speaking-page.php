65314b935feec12e4a776d24f887c130c88fdac7
Alain Schlesser <alain.schlesser@gmail.com>
2017-08-12 20:19:30 +0200

    Split talks into upcoming and past talks.

--- views/speaking-page.php
+++ views/speaking-page.php
@@ -11,9 +11,20 @@
 
 namespace AlainSchlesser\Speaking;
 
-?><h3>Speaking page view</h3>
-<div class="speaking-page-talks">
+$upcoming_header = false;
+$past_header     = false;
+$timestamp       = time();
+
+?><div class="speaking-page-talks">
 	<?php foreach ( $this->talks as $talk ) : ?>
-		<?= $this->render_partial( 'views/speaking-page-talk', [ 'talk' => $talk ] ) ?>
+		<?php if ( ! $upcoming_header && $talk->get_session_date() >= $timestamp ) { ?>
+			<h3><?= __( 'Upcoming Talks', 'as-speaking' ) ?></h3>
+			<?php $upcoming_header = true; ?>
+		<?php } else if ( ! $past_header && $talk->get_session_date() < $timestamp ) { ?>
+			<h3><?= __( 'Past Talks', 'as-speaking' ) ?></h3>
+			<?php $past_header = true; ?>
+		<?php } ?>
+		<?= $this->render_partial( 'views/speaking-page-talk',
+			[ 'talk' => $talk ] ) ?>
 	<?php endforeach; ?>
 </div>
