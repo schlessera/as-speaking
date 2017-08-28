0ce8670956728f8843e43a125ac0f74375bfb36f
Alain Schlesser <alain.schlesser@gmail.com>
2017-08-28 18:59:10 +0200

    Fix docblock.

--- src/Model/CustomPostTypeRepository.php
+++ src/Model/CustomPostTypeRepository.php
@@ -22,11 +22,11 @@ namespace AlainSchlesser\Speaking\Model;
 abstract class CustomPostTypeRepository {
 
 	/**
-	 * Persist a modified Talk to the storage.
+	 * Persist a modified entity to the storage.
 	 *
 	 * @since 0.1.0
 	 *
-	 * @param CustomPostTypeEntity $entity
+	 * @param CustomPostTypeEntity $entity Entity instance to persist.
 	 */
 	public function persist( CustomPostTypeEntity $entity ) {
 		wp_insert_post( $entity->get_post_object()->to_array() );
