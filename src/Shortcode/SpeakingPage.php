e11e9d48d59a3c24550a0532751c3804fc306000
Alain Schlesser <alain.schlesser@gmail.com>
2017-10-28 16:15:19 +0200

    Reorder upcoming talks.
    Upcoming talks should be sorted in ascending order (soonest first), while past talks should be sorted in descending order.
    
    Fixes #2

--- src/Shortcode/SpeakingPage.php
+++ src/Shortcode/SpeakingPage.php
@@ -12,7 +12,9 @@
 namespace AlainSchlesser\Speaking\Shortcode;
 
 use AlainSchlesser\Speaking\Assets\StyleAsset;
+use AlainSchlesser\Speaking\Model\Talk;
 use AlainSchlesser\Speaking\Model\TalkRepository;
+use DateTime;
 
 /**
  * Class SpeakingPage.
@@ -63,8 +65,10 @@ final class SpeakingPage extends BaseShortcode {
 	protected function get_context( $atts ) {
 		$talk_repository = new TalkRepository();
 
+		$talks = $talk_repository->find_all();
+
 		return [
-			'talks' => $talk_repository->find_all(),
+			'talks' => $this->sort_talks( $talks ),
 		];
 	}
 
@@ -82,5 +86,46 @@ final class SpeakingPage extends BaseShortcode {
 				self::CSS_URI
 			),
 		];
+	}
+
+	/**
+	 * Sort the array of talks for display.
+	 *
+	 * @since 0.2.12
+	 *
+	 * @param array <Talk> $talks Array of talks to sort.
+	 *
+	 * @return array<Talk> Sorted array of talks.
+	 */
+	protected function sort_talks( $talks ) {
+		$now = ( new DateTime() )->getTimestamp();
+
+		// We need a custom sort function to reverse the sort order for
+		// upcoming talks (soonest first) as opposed to past talks.
+		usort(
+			$talks,
+			function ( Talk $talk_a, Talk $talk_b ) use ( $now ) {
+				$timestamp_a = $talk_a->get_session_date();
+				$timestamp_b = $talk_b->get_session_date();
+
+				if ( $timestamp_a >= $now ) {
+					// A is upcoming.
+					return $timestamp_b >= $now
+						// B is also upcoming, ascend natural number.
+						? strnatcmp( $timestamp_a, $timestamp_b )
+						// B is past.
+						: - 1;
+				}
+
+				// A is past.
+				return $timestamp_b >= $now
+					// B is upcoming.
+					? 1
+					// B is also past, descend on natural number.
+					: strnatcmp( $timestamp_b, $timestamp_a );
+			}
+		);
+
+		return $talks;
 	}
 }
